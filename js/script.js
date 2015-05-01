e99835d77ebca731e678b64dbea04fafb2ce67e6
Minh Tran <max.thien.tran@gmail.com>
2015-05-01 08:52:49 -0700

    picture showed up

--- js/script.js
+++ js/script.js
@@ -25,19 +25,23 @@ console.log(namePicker); */
 	function randomName(){
 
 		if (wdi_16_names.length <1) {
-			wdi_16_names = ["Alexander Mireles", "Andre Trelles", "Andrew Ham", 
-			"Ari Kanamori", "Busyra Anwar", "Carlos Meza", "Chris Garvin", 
+			wdi_16_names = [{name: "Alexander Mireles", pic: "http://blog.africadreamsafaris.com/wp-content/uploads/2011/09/Giraffe-Sparring.jpg"}, 
+							{name: "Andre Trelles", pic:"http://funmozar.com/wp-content/uploads/2015/02/Monkey-36.jpg"}, 
+							{name:"Andrew Ham", pic: "http://funmozar.com/wp-content/uploads/2015/02/Monkey-32.jpg"}];
+			/* "Ari Kanamori", "Busyra Anwar", "Carlos Meza", "Chris Garvin", 
 			"Cristal Chavez", "Daley Meistrell", "Daniel Manuel", "Jefferson Choi",
 			 "Jessica Grinberg", "John Ward", "Kate Wood", "Kevin Mehrabi", "Max Tran",
 			  "Max Plisskin", "Megan Kakitsubo", "Meryl Harden", "Mike McCready", 
 			  "Nick Dalke","Niels Bischoff", "Paul Aglione", "Rahul Sidhu", 
-			  "Samuel Chun", "Samuel Amiri", "Sophie Chor","Steve Soter", "Taylor Adams", "Yo Wakita"]
+			  "Samuel Chun", "Samuel Amiri", "Sophie Chor","Steve Soter", "Taylor Adams", "Yo Wakita"] */
 		}
-		var pick = [Math.floor(Math.random() * wdi_16_names.length)];
-		var namePicker = wdi_16_names[pick];
-		document.getElementById("name").innerHTML = namePicker;
-		wdi_16_names.splice(pick,1); //eliminating 
-		console.log(namePicker);
+		var pick = Math.floor(Math.random() * wdi_16_names.length)
+	    var namePicker = wdi_16_names[pick].name;
+	    var picPicker = wdi_16_names[pick].pic;
+	    document.getElementById('displayName').innerHTML = namePicker;
+	    document.getElementById('randomPicture').style.backgroundImage= 'url(' + picPicker + ')';
+	    wdi_16_names.splice(pick,1);  
+	    console.log(namePicker);
 
 	}
 
